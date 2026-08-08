bf4faed60e748bd88e0d376012f4b545af11a51c
eye-0019 <gzbrkdww2g@privaterelay.appleid.com>
2026-08-07 23:12:46 -0500

    Create addrole.js

---
+++ src/commands/Moderation/addrole.js
@@ -0,0 +1,45 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { logAction } = require('../../utils/logAction');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('addrole')
+        .setDescription('Add a role to a member')
+        .addUserOption(option =>
+            option.setName('target')
+                .setDescription('The member to give the role to')
+                .setRequired(true))
+        .addRoleOption(option =>
+            option.setName('role')
+                .setDescription('The role to add')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('reason')
+                .setDescription('Reason for adding this role')
+                .setRequired(false))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
+
+    async execute(interaction) {
+        const target = interaction.options.getMember('target');
+        const role = interaction.options.getRole('role');
+        const reason = interaction.options.getString('reason') || 'No reason provided';
+
+        if (!target) {
+            return interaction.reply({ content: 'Could not find that member.', ephemeral: true });
+        }
+        if (target.roles.cache.has(role.id)) {
+            return interaction.reply({ content: `${target.user.tag} already has that role.`, ephemeral: true });
+        }
+
+        await target.roles.add(role, reason);
+        await logAction(interaction.guild, {
+            action: 'Role Added',
+            moderator: interaction.user,
+            target: target.user,
+            reason: `${reason} (role: ${role.name})`,
+            color: 0x2ECC71
+        });
+
+        return interaction.reply(`Added ${role} to ${target.user.tag}. Reason: ${reason}`);
+    }
+};
