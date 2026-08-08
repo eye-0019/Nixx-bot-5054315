85e491eebb8c5bff0f5de032c603f04b34172ef8
eye-0019 <gzbrkdww2g@privaterelay.appleid.com>
2026-08-07 23:13:49 -0500

    Add guildMemberAdd event for logging new members

---
+++ src/events/guildMemberAdd.js
@@ -0,0 +1,18 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'guildMemberAdd',
+    async execute(member) {
+        const logChannelId = process.env.LOG_CHANNEL_ID;
+        const channel = member.guild.channels.cache.get(logChannelId);
+        if (!channel) return;
+
+        const embed = new EmbedBuilder()
+            .setTitle('Member Joined')
+            .setColor(0x2ECC71)
+            .setDescription(`${member.user.tag}`)
+            .setTimestamp();
+
+        channel.send({ embeds: [embed] }).catch(() => {});
+    }
+};
