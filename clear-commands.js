3611a8f8f415d3e24e02c3ac32dd32e369594238
eye-0019 <gzbrkdww2g@privaterelay.appleid.com>
2026-08-08 19:55:01 -0500

    Update clear-commands.js

--- clear-commands.js
+++ clear-commands.js
@@ -9,7 +9,13 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
       Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
       { body: [] }
     );
-    console.log('All guild commands cleared.');
+    console.log('Guild commands cleared.');
+
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: [] }
+    );
+    console.log('Global commands cleared.');
   } catch (err) {
     console.error(err);
   }
