2ef54736e6b1d5b8bc4e5728b86d51119ed396fa
eye-0019 <gzbrkdww2g@privaterelay.appleid.com>
2026-08-09 10:39:07 -0500

    Update messageDelete.js

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -13,9 +13,7 @@ module.exports = {
         const sentTime = `<t:${Math.floor(message.createdTimestamp / 1000)}:F>`;
 
         const embed = new EmbedBuilder()
-            .setColor(0xFFFFFF)
-            .setAuthor({ name: 'Message Deleted' })
-            .setThumbnail(message.author?.displayAvatarURL() || null)
+            .setAuthor({ name: 'Message Deleted', iconURL: message.author?.displayAvatarURL() || null })
             .setDescription(`Message from ${message.author} deleted in ${message.channel}\nit was sent at ${sentTime}\n[Jump to message](${message.url})`)
             .addFields(
                 { name: 'Message Content', value: message.content?.slice(0, 1000) || '*No content*' },
