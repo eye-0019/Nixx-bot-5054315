795eb3508082cdcd997e1d74bc51d677260487ab
eye-0019 <gzbrkdww2g@privaterelay.appleid.com>
2026-08-08 22:10:57 -0500

    Change embed color on member join event

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -9,7 +9,7 @@ module.exports = {
 
         const embed = new EmbedBuilder()
             .setTitle('Member Joined')
-            .setColor(0x2ECC71)
+            .setColor(0x95A5A6)
             .setDescription(`${member.user.tag}`)
             .setTimestamp();
 
