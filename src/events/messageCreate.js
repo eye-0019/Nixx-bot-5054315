31d2ef170c005e68cbb1a16fc0acfad2205ede72
eye-0019 <gzbrkdww2g@privaterelay.appleid.com>
2026-08-08 22:14:54 -0500

    Change embed color for DM reply notification

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -18,7 +18,7 @@ module.exports = {
 
         const embed = new EmbedBuilder()
             .setTitle('DM Reply Received')
-            .setColor(0x3498DB)
+            .setColor(0x95A5A6)
             .addFields(
                 { name: 'From', value: `${message.author.tag}` },
                 { name: 'Message', value: message.content?.slice(0, 1000) || '*No content*' }
